61a417db2b8b01a0a5322f374c742073501f0b59
jaguar-ks <fahdamineksouss@gmail.com>
2023-09-25 07:53:19 +0100

    RESTART : reworking the last exirsice

---
+++ ex03/src/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faksouss <faksouss@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 07:52:03 by faksouss          #+#    #+#             */
+/*   Updated: 2023/09/25 07:52:25 by faksouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+int main()
+{
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    ICharacter* me = new Character("me");
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+    delete bob;
+    delete me;
+    delete src;
+    return 0;
+}
