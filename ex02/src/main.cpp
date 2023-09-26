d968fa9bdc9018504635bbc0939c081fddc01434
jaguar-ks <fahdamineksouss@gmail.com>
2023-09-26 08:37:47 +0100

    UPDATE : edited the main file

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: faksouss <faksouss@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 03:11:36 by faksouss          #+#    #+#             */
-/*   Updated: 2023/09/24 05:39:02 by faksouss         ###   ########.fr       */
+/*   Updated: 2023/09/26 08:37:02 by faksouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,8 @@
 int main (){
     // atexit(lol);
     const Animal *array[10];
-    for (int i = 0; i < 5; i++)
-        array[i] = new Dog();
-    for (int i = 5; i < 10; i++)
-        array[i] = new Cat();
+    for (int i = 0; i < 10; i++)
+        i < 5 ? array[i] = new Dog() : array[i] = new Cat();
     for (int i = 0; i < 10; i++)
         array[i]->makeSound();
     for (int i = 0; i < 10; i++)
